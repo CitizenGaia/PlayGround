8b7093bb14037641263a24a390d9245c4e98b823
Michael Broe <michael-broe.andreasen@capgemini.com>
2018-02-03 21:18:51 +0100

    improve test

--- utils/src/main/java/CVSUtils.java
+++ utils/src/main/java/CVSUtils.java
@@ -1,21 +1,29 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+// TODO: Fix bugs in this code from www.mkyong.com
 public class CVSUtils {
-
-    private Scanner scanner;
-    private boolean loaded = false;
 
     private char separator = ',';
     private char quote = '"';
+    private String regexp = "^(\\[[1-9]\\]){3}(\\n)?$";
+
+    private List<String> extracted;
+    private boolean loaded = false;
+
+    private String details = "";
+    private Pattern pattern;
 
     public boolean load(File cvsFile) {
         try {
-            scanner = new Scanner(cvsFile);
-            loaded = true;
+            Scanner scanner = new Scanner(cvsFile);
+            loaded = extractData(scanner);
+            if (!loaded) {
+                System.out.println((String.format("Line:\n%s\n does'nt match RegExp:%s", details, regexp)));
+            }
         } catch (FileNotFoundException e) {
             System.out.println("File not found" + e.getMessage());
         }
@@ -38,19 +46,54 @@ public class CVSUtils {
         this.quote = quote;
     }
 
-    public String dump() {
-        StringBuilder builder = new java.lang.StringBuilder();
+    public String getRegexp() {
+        return regexp;
+    }
+
+    public void setRegexp(String regexp) {
+        this.regexp = regexp;
+    }
+
+    private boolean extractData(Scanner scanner) throws IllegalFormatException {
+        extracted = new ArrayList<String>();
+        Matcher matcher;
+        pattern = Pattern.compile(regexp);
+
+        boolean isValid = true;
         while (scanner.hasNext()) {
-            List<String> line = parseLine(scanner.nextLine());
-            int i = 0;
-            for (String value: line) {
+            String line = scanner.nextLine();
+            List<String> parsedLine = parseLine(line);
+            StringBuilder builder = new java.lang.StringBuilder();
+            for (String value : parsedLine) {
                 builder.append("[");
                 builder.append(value);
                 builder.append("]");
             }
-            builder.append("\n")
+            builder.append("\n");
+
+            matcher = pattern.matcher(builder.toString());
+            if (!matcher.matches()) {
+                isValid = false;
+                details = builder.toString();
+            } else {
+                System.out.print(matcher.group(0));
+            }
+            extracted.add(builder.toString());
         }
         scanner.close();
+        return isValid;
+    }
+
+    public String dump() {
+        StringBuilder builder = new java.lang.StringBuilder();
+        Iterator<String> lines = extracted.iterator();
+        while (lines.hasNext()) {
+            List<String> valuesInLine = parseLine(lines.next());
+            for (String value : valuesInLine) {
+                builder.append(value);
+            }
+            builder.append("\n");
+        }
         return builder.toString();
     }
 
@@ -61,7 +104,7 @@ public class CVSUtils {
     private List<String> parseLine(String cvsLine, char separators, char customQuote) {
         List<String> result = new ArrayList<String>();
         //if empty, return!
-        if (cvsLine == null && cvsLine.isEmpty()) {
+        if (cvsLine == null || cvsLine.length()==0) {
             return result;
         }
         StringBuffer curVal = new StringBuffer();
