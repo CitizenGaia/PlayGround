4fbf95c966d5e26744ac9da2df198492aac68115
Michael Broe <michael-broe.andreasen@capgemini.com>
2018-02-12 14:43:11 +0100

    refactor

---
+++ utils/src/main/java/GSONUtils.java
@@ -0,0 +1,21 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.Map;
+
+public class GSONUtils {
+
+    private Gson gson;
+
+    public GSONUtils() {
+        gson = new Gson();
+    }
+
+    public String gameToJson(Map<Integer, Integer> game) {
+        return gson.toJson(game);
+    }
+
+    public Map<Integer, Integer> jsonToGame(String sourceGame) {
+        return gson.fromJson(sourceGame, new TypeToken<Map<Integer, Integer>>(){}.getType());
+    }
+}
---
+++ utils/src/test/java/GSONUtilsTest.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class GSONUtilsTest {
+
+    private GSONUtils utils;
+    private final static Map<Integer, Integer> game = Collections.unmodifiableMap(new HashMap<Integer, Integer>() {
+        {
+            put(1, 1); put(2, 2); put(3, 3);
+            put(10, 2); put(11, 3); put(12, 4);
+            put(20, 3); put(21, 4); put(22, 5);
+        }
+    });
+
+    @Before
+    public void setup() {
+        utils = new GSONUtils();
+    }
+
+    @Test
+    public void gameToJsonAndBack() {
+        String jsonFormattedGame = utils.gameToJson(game);
+        Map<Integer, Integer> gameFromJson = utils.jsonToGame(jsonFormattedGame);
+        assertTrue("game should be the same after transformation to JSON and back again", game.equals(gameFromJson));
+    }
+
+}
